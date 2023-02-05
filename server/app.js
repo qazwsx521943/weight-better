d45d3c24def97247a6757a76ff85a30b1aad4f32
KrisYan <krisyan3318@gmail.com>
2023-02-05 17:44:35 +0800

    products 後端 api -- version 1 1. 取得所有 Level1 產品資料 2. 取得單一 Level1 產品資料 3. 取得單一 Level2 產品資料 4. 取得單一 類別 產品資料

--- server/app.js
+++ server/app.js
@@ -5,6 +5,12 @@ const bodyParser = require("body-parser");
 const feedRoutes = require("./routes/feed");
 // const feedRoutes = require("./controllers/feed");
 
+// --[使用 .env(預設) 的環境變數]
+require('dotenv').config()
+
+// --[連線資料庫]
+const db = require('./modules/connect-mysql')
+
 const app = express();
 
 app.use(bodyParser.json()); // NOTE aplication/json
@@ -19,4 +25,10 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoutes);
 
-app.listen(8080);
+// --[建立 products 路由]
+const products = require('./routes/products')
+app.use('/products', products)
+
+app.listen(8080, () => {
+    console.log(`server run on port ${8080}`)
+});

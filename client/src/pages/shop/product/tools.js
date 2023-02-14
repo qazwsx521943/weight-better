dfdea82f9dd5fb0390cd40db5714e8246e97e720
yingchuniu <yuni85113@gmail.com>
2023-02-14 16:08:13 +0800

    creat tools

---
+++ client/src/pages/shop/product/tools.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import { Grid } from "@mui/material";
+
+function rearchTools() {
+    return (
+        <div></div>
+        
+    );
+}
+
+export default rearchTools;

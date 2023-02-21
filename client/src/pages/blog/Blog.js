c6d205eab401988673c8c0e62f1612c82776e884
HanYi17 <henryhuang52011@hotmail.com>
2023-02-21 16:37:44 +0800

    0221

--- client/src/pages/blog/Blog.js
+++ client/src/pages/blog/Blog.js
@@ -1,129 +1,169 @@
-import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Header from './Header';
-import MainFeaturedPost from './MainFeaturedPost';
-import FeaturedPost from './FeaturedPost';
-import Main from './Main';
-import Sidebar from './Sidebar';
-import Footer from './Footer';
-import post1 from './blog-post.1.md';
-import post2 from './blog-post.2.md';
-import post3 from './blog-post.3.md';
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import { Box } from "@mui/system";
+import Container from "@mui/material/Container";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Header from "./Header";
+import MainFeaturedPost from "./MainFeaturedPost";
+import FeaturedPost from "./FeaturedPost";
+import Main from "./Main";
+import Sidebar from "./Sidebar";
+import Footer from "./Footer";
+import post1 from "./blog-post.1.md";
+import post2 from "./blog-post.2.md";
+import post3 from "./blog-post.3.md";
 
 const sections = [
-  { title: '最新文章', url: '#' },
-  { title: '健身鍛鍊', url: '#' },
-  { title: '居家運動', url: '#' },
-  { title: '健康飲食', url: '#' },
-  { title: '養生保健', url: '#' },
+    { title: "最新文章", url: "#" },
+    { title: "健身鍛鍊", url: "#" },
+    { title: "居家運動", url: "#" },
+    { title: "健康飲食", url: "#" },
+    { title: "養生保健", url: "#" },
 ];
 
 const mainFeaturedPost = {
-  title: '原來運動可以不用這麼辛苦？',
-  description:
-    "享受運動，維持身心健康，讓運動成為生活中的一部分。",
-  image: 'https://source.unsplash.com/random/?workout',
-  imageText: 'main image description',
-  // linkText: 'Continue reading…',
+    title: "原來運動可以不用這麼辛苦？",
+    description: "享受運動，維持身心健康，讓運動成為生活中的一部分。",
+    image: "https://source.unsplash.com/random/?workout",
+    imageText: "main image description",
+    // linkText: 'Continue reading…',
 };
 
 const featuredPosts = [
-  {
-    title: '滑雪好處竟然這麼多!?',
-    date: 'Nov 12',
-    description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: 'https://source.unsplash.com/random/?skiing',
-    imageLabel: 'Image Text',
-  },
-  {
-    title: '多樣食材運用好吃驚!',
-    date: 'Nov 11',
-    description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: 'https://source.unsplash.com/random/?ingredients',
-    imageLabel: 'Image Text',
-  },
-  {
-    title: '孕婦也能做的五種運動',
-    date: 'Nov 11',
-    description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: 'https://source.unsplash.com/random/?pregnant',
-    imageLabel: 'Image Text',
-  },
-  {
-    title: '五分鐘學會健康吃',
-    date: 'Nov 11',
-    description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: 'https://source.unsplash.com/random/?diet',
-    imageLabel: 'Image Text',
-  },
+    {
+        title: "滑雪好處竟然這麼多!?",
+        // date: 'Nov 12',
+        // description:
+        //   'This is a wider card with supporting text below as a natural lead-in to additional content.',
+        image: "https://source.unsplash.com/random/500x500/?skiing",
+        imageLabel: "Image Text",
+    },
+    {
+        title: "多樣食材運用好吃驚!",
+        // date: 'Nov 11',
+        // description:
+        //   'This is a wider card with supporting text below as a natural lead-in to additional content.',
+        image: "https://source.unsplash.com/random/500x500/?ingredients",
+        imageLabel: "Image Text",
+    },
+    {
+        title: "孕婦也能做的五種運動",
+        // date: 'Nov 11',
+        // description:
+        //   'This is a wider card with supporting text below as a natural lead-in to additional content.',
+        image: "https://source.unsplash.com/random/500x500/?pregnant",
+        imageLabel: "Image Text",
+    },
+    {
+        title: "五分鐘學會健康吃",
+        // date: 'Nov 11',
+        // description:
+        //   'This is a wider card with supporting text below as a natural lead-in to additional content.',
+        image: "https://source.unsplash.com/random/500x500/?Healthy+Meals",
+        imageLabel: "Image Text",
+    },
+    {
+        title: "從事運動沒想到這麼簡單！",
+        // date: 'Nov 11',
+        description:
+            "避免於太飽或太餓時做運動。不應空腹做運動，特別緊記要吃早餐，以免體力不支。游泳前不要喝過多飲料，以免因嘔吐而導致哽塞。避免於太飽或太餓時做運動。不應空腹做運動，特別緊記要吃早餐，以免體力不支。游泳前不要喝過多飲料，以免因嘔吐而導致哽塞。",
+        image: "https://source.unsplash.com/random/500x500/?sports",
+        imageLabel: "Image Text",
+    },
 ];
+const fourPosts = featuredPosts.slice(0, 4);
+const threePosts = featuredPosts.slice(0, 3);
+const post = featuredPosts.slice(4, 5);
 
 const posts = [post1, post2, post3];
 
 const sidebar = {
-  title: 'About',
-  description:
-    'Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.',
-  archives: [
-    { title: 'March 2020', url: '#' },
-    { title: 'February 2020', url: '#' },
-    { title: 'January 2020', url: '#' },
-    { title: 'November 1999', url: '#' },
-    { title: 'October 1999', url: '#' },
-    { title: 'September 1999', url: '#' },
-    { title: 'August 1999', url: '#' },
-    { title: 'July 1999', url: '#' },
-    { title: 'June 1999', url: '#' },
-    { title: 'May 1999', url: '#' },
-    { title: 'April 1999', url: '#' },
-  ],
-  social: [
-    { name: 'GitHub', icon: GitHubIcon },
-    { name: 'Twitter', icon: TwitterIcon },
-    { name: 'Facebook', icon: FacebookIcon },
-  ],
+    title: "About",
+    description:
+        "Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.",
+    archives: [
+        { title: "March 2020", url: "#" },
+        { title: "February 2020", url: "#" },
+        { title: "January 2020", url: "#" },
+        { title: "November 1999", url: "#" },
+        { title: "October 1999", url: "#" },
+        { title: "September 1999", url: "#" },
+        { title: "August 1999", url: "#" },
+        { title: "July 1999", url: "#" },
+        { title: "June 1999", url: "#" },
+        { title: "May 1999", url: "#" },
+        { title: "April 1999", url: "#" },
+    ],
+    social: [
+        { name: "GitHub", icon: GitHubIcon },
+        { name: "Twitter", icon: TwitterIcon },
+        { name: "Facebook", icon: FacebookIcon },
+    ],
 };
 
-const theme = createTheme();
-
 export default function Blog() {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Container maxWidth="100%">
-        <Header title="Blog" sections={sections} />
-        <main>
-          <MainFeaturedPost post={mainFeaturedPost} />
-          <Grid container spacing={4}>
-            {featuredPosts.map((post) => (
-              <FeaturedPost key={post.title} post={post} />
-            ))}
-          </Grid>
-          <Grid container spacing={5} sx={{ mt: 3 }}>
-            <Main title="From the firehose" posts={posts} />
-            <Sidebar
-              title={sidebar.title}
-              description={sidebar.description}
-              archives={sidebar.archives}
-              social={sidebar.social}
+    return (
+        <Grid>
+            <Header title="Blog" sections={sections} />
+            <MainFeaturedPost post={mainFeaturedPost} />
+            <Grid sx={{ mx: 0 }}>
+                <Grid container sx={{ p: 0, my: 3 }}>
+                    {fourPosts.map((post) => (
+                        <FeaturedPost
+                            cardStyle="normalCard"
+                            actionAreaStyle="normalActionArea"
+                            mediaStyle="normalMedia"
+                            contentStyle="normalContent"
+                            key={post.title}
+                            post={post}
+                        />
+                    ))}
+                </Grid>
+                    {post.map((post) => (
+                        <Box key={post.title} sx={{ p: 0, mr: 3 }}>
+                            <FeaturedPost
+                                cardStyle="largeCard"
+                                actionAreaStyle="largeActionArea"
+                                mediaStyle="largeMedia"
+                                contentStyle="largeContent"
+                                post={post}
+                            />
+                        </Box>
+                    ))}
+                <Grid container sx={{ p: 0, my: 3 }}>
+                    {threePosts.map((post) => (
+                        <Grid key={post.title} sx={{ p: 0, mx: 3 }}>
+                            <FeaturedPost
+                                cardStyle="smallCard"
+                                actionAreaStyle="smallActionArea"
+                                mediaStyle="smallMedia"
+                                contentStyle="smallContent"
+                                post={post}
+                            />
+                        </Grid>
+                    ))}
+                    
+                </Grid>
+                    
+            </Grid>
+            {/* <Grid container spacing={5} sx={{ mt: 3 }}>
+                <Main title="From the firehose" posts={posts} />
+                <Sidebar
+                    title={sidebar.title}
+                    description={sidebar.description}
+                    archives={sidebar.archives}
+                    social={sidebar.social}
+                />
+            </Grid> */}
+
+            <Footer
+                title="Footer"
+                description="Something here to give the footer a purpose!"
             />
-          </Grid>
-        </main>
-      </Container>
-      <Footer
-        title="Footer"
-        description="Something here to give the footer a purpose!"
-      />
-    </ThemeProvider>
-  );
+        </Grid>
+    );
 }
--- client/src/pages/blog/FeaturedPost.js
+++ client/src/pages/blog/FeaturedPost.js
@@ -1,55 +1,112 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import Card from '@mui/material/Card';
-import CardActionArea from '@mui/material/CardActionArea';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import { Divider } from '@mui/material';
+import * as React from "react";
+import PropTypes from "prop-types";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import { Divider } from "@mui/material";
 
 function FeaturedPost(props) {
-  const { post } = props;
-
-  return (
-    <Grid item xs={12} md={3}>
-      <CardActionArea component="a" href="#">
-        <Card sx={{ display: 'flex', flexDirection: 'column',width: `320px`, height: `250px` }}>
-        <CardMedia
-            component="img"
-            sx={{ width: `320px`, height: `200px` }}
-            image={post.image}
-            alt={post.imageLabel}
-          />
-          <Divider />
-          <CardContent sx={{ flex: 1,width: `320px` }}>
-            <Typography component="h2" variant="h5">
-              {post.title}
-            </Typography>
-            <Typography variant="subtitle1" color="text.secondary">
-              {post.date}
-            </Typography>
-            <Typography variant="subtitle1" paragraph>
-              {post.description}
-            </Typography>
-            <Typography variant="subtitle1" color="primary">
-              Continue reading...
-            </Typography>
-          </CardContent>
-        </Card>
-      </CardActionArea>
-    </Grid>
-  );
+    const { post, cardStyle, actionAreaStyle, mediaStyle,contentStyle } = props;
+    const cardStyles = {
+        normalCard: {
+            display: "flex",
+            flexDirection: "column",
+            width: `320px`,
+            height: `250px`,
+        },
+        largeCard: {
+            display: "flex",
+            flexDirection: "column",
+            width: `450px`,
+            height: `800px`,
+        },
+        smallCard: {
+            display: "flex",
+            flexDirection: "column",
+            width: `200px`,
+            height: `225px`,
+        },
+    };
+    const actionAreaStyles = {
+        normalActionArea: {
+            width: `320px`,
+            height: `250px`,
+        },
+        largeActionArea: {
+            width: `450px`,
+            height: `800px`,
+        },
+        smallActionArea: {
+            width: `200px`,
+            height: `225px`,
+        },
+    };
+    const mediaStyles = {
+        normalMedia: {
+            width: `320px`,
+            height: `200px`,
+        },
+        largeMedia: {
+            width: `450px`,
+            height: `450px`,
+        },
+        smallMedia: {
+            width: `320px`,
+            height: `250px`,
+        },
+    };
+    const contentStyles = {
+        normalContent: { flex: 1, width: `320px` },
+        largeContent: { flex: 1, width: `450px` },
+        smallContent: { flex: 1, width: `320px` },
+    };
+    return (
+        <Grid item xs={12} sm={6} lg={3} sx={{ maxWidth: "320px" }}>
+            <CardActionArea
+                component="a"
+                href="#"
+                sx={{ ...actionAreaStyles[actionAreaStyle] }}
+            >
+                <Card sx={{ ...cardStyles[cardStyle] }}>
+                    <CardMedia
+                        component="img"
+                        sx={{ ...mediaStyles[mediaStyle] }}
+                        image={post.image}
+                        alt={post.imageLabel}
+                    />
+                    <Divider />
+                    <CardContent sx={{ ...contentStyles[contentStyle] }}>
+                        <Typography component="h2" variant="h4">
+                            {post.title}
+                        </Typography>
+                        {/* <Typography variant="subtitle1" color="text.secondary">
+                            {post.date}
+                        </Typography> */}
+                        <Divider sx={{my:1}}/>
+                        <Typography variant="subtitle2" paragraph>
+                            {post.description}
+                        </Typography>
+                        <Typography variant="subtitle1" color="primary">
+                            Continue reading...
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </CardActionArea>
+        </Grid>
+    );
 }
 
 FeaturedPost.propTypes = {
-  post: PropTypes.shape({
-    date: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    imageLabel: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-  }).isRequired,
+    post: PropTypes.shape({
+        date: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        imageLabel: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+    }).isRequired,
 };
 
 export default FeaturedPost;

3abc8d53b7d812a1db236a960324ea34a78946c2
KrisYan <krisyan3318@gmail.com>
2023-02-22 19:02:25 +0800

    fix home

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import { useAuth } from "./hooks/AuthContext";
 // import Login from "./pages/login";
 import Login from "./pages/authentication/Login";
 import Register from "./pages/authentication/Register";
-import Home from "./pages/home/Home";
+import Home from "./pages/home/home";
 import ErrorPage from "./pages/ErrorPage";
 import AuthRequired from "./pages/authentication/RequireAuth";
 

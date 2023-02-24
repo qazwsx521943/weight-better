1b2b3b4085805471bdcc58b7ee64cc2e7a04a2f8
KrisYan <krisyan3318@gmail.com>
2023-02-24 14:21:09 +0800

    align bootstrap screen size

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -30,6 +30,18 @@ module.exports = {
             h7: '12px',
             h8: '10px',
         },
+        screens: {
+            'sm': '576px',
+            // => @media (min-width: 640px) { ... }
+            'md': '768px',
+            // => @media (min-width: 768px) { ... }
+            'lg': '992px',
+            // => @media (min-width: 1024px) { ... }
+            'xl': '1200px',
+            // => @media (min-width: 1280px) { ... }
+            '2xl': '1536px',
+            // => @media (min-width: 1536px) { ... }
+        }
     },
 
     plugins: [],
